53f2821c758fd9ec0ad49bb17e8e8564d255b046
Kaíque Zimerer <kaiquezimerer@outlook.com>
2021-12-23 04:11:48 -0300

    Config. Stripe SDK methods + getServerSideProps() to SSR calls

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
+import { GetServerSideProps } from 'next';
+
 import Head from 'next/head';
 
 import { SubscribeButton  } from '../components/SubscribeButton';
+import { stripe } from '../services/stipe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -16,12 +26,32 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
 }
+
+// SSR Props
+export const getServerSideProps: GetServerSideProps = async () => {
+  // Stripe SDK
+  const price = await stripe.prices.retrieve('price_1K9kzdJBZZDXIB5eN66icTu3');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    }
+  };
+};
