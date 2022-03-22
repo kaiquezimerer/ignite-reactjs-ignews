b5a37f0ac23b544387c115e74251f6119c443952
Kaíque Zimerer <kaiquezimerer@outlook.com>
2022-03-22 13:04:13 -0300

    Add tests for SubscribeButton component

---
+++ src/components/SubscribeButton/SubcribeButton.spec.tsx
@@ -0,0 +1,67 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { mocked } from 'ts-jest/utils';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+
+import { SubscribeButton } from '.';
+
+// Mocking modules (next-auth/react)
+jest.mock('next-auth/react');
+
+jest.mock('next/router');
+
+describe('SubscribeButton component', () => {
+  it('renders correctly when user is not authenticated', () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
+    render(<SubscribeButton />);
+
+    expect(screen.getByText('Subscribe now')).toBeInTheDocument();
+  });
+
+  it('redirects user to sign in when not authenticated', () => {
+    const signInMocked = mocked(signIn);
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
+    render(<SubscribeButton />);
+
+    const subscribeButton = screen.getByText('Subscribe now');
+
+    fireEvent.click(subscribeButton);
+
+    expect(signInMocked).toHaveBeenCalled();
+  });
+
+  it('redirects to posts when user already has a subcription', () => {
+    const useRouterMocked = mocked(useRouter);
+    const useSessionMocked = mocked(useSession);
+    const pushMock = jest.fn();
+
+    useSessionMocked.mockReturnValueOnce({
+      data: {
+        user: {
+          name: 'John Doe', email: 'john.doe@example.com'
+        },
+        activeSubscription: 'fake-active-subscription',
+        expires: 'fake-expires'
+      },
+      status: 'authenticated'
+    })
+
+    useRouterMocked.mockReturnValueOnce({
+      push: pushMock,
+    } as any);
+
+    render(<SubscribeButton />);
+
+    const subscribeButton = screen.getByText('Subscribe now');
+
+    fireEvent.click(subscribeButton);
+
+    expect(pushMock).toHaveBeenCalledWith('/posts');
+  });
+});
